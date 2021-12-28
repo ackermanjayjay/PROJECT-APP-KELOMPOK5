225db87a524bc1c444d1373ba0c410bcd372d5df
ackermanjayjay <reza23198@gmail.com>
2021-12-28 18:55:38 +0700

    menambahkan acitvity baru

---
+++ Coba_Firebase/app/src/main/java/com/example/coba_firebase/Halaman_transaksi.java
@@ -0,0 +1,14 @@
+package com.example.coba_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Halaman_transaksi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_transaksi);
+    }
+}
