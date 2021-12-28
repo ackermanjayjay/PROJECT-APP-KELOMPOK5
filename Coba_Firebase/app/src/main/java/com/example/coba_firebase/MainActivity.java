a6993846080dc4ced604e6b2b8470573ba3e5e66
ackermanjayjay <reza23198@gmail.com>
2021-12-28 18:42:24 +0700

    Pengubahan tampilan

--- Coba_Firebase/app/src/main/java/com/example/coba_firebase/MainActivity.java
+++ Coba_Firebase/app/src/main/java/com/example/coba_firebase/MainActivity.java
@@ -9,71 +9,86 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-Button login,register;
-EditText nama,pass_ku;
-String admin="Reza";
+    Button masuk;
+    EditText nama, pass_ku;
+    String admin = "Reza";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nama=findViewById(R.id.txt_nama);
-        pass_ku=findViewById(R.id.txt_pass);
-        login=findViewById(R.id.btn_login);
-        register=findViewById(R.id.btn_regsiter);
-        getSupportActionBar().setTitle("Halaman Login");
+//        nama=findViewById(R.id.txt_nama);
+//        pass_ku=findViewById(R.id.txt_pass);
+//        login=findViewById(R.id.btn_login);
+//        register=findViewById(R.id.btn_regsiter);
+        getSupportActionBar().setTitle("Toko Sembako Kelompok 5");
+        masuk=(Button)findViewById(R.id.btn_masuk);
 
-        register.setOnClickListener((new View.OnClickListener() {
+        masuk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                halamanRegister();
-            }
-        }));
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Control khusus Admin login
-                    if(nama.getText().toString().equals("Reza")) {
-                        Intent admin = new Intent(MainActivity.this, Halaman_admin.class);
-                        admin.putExtra("Nama", nama.getText().toString());
-                        startActivity(admin);
-                    }
-                 else
-                    {
-                    halamanhome();
-                }
+                halamanhome();
             }
         });
-    }
-    public void halamanRegister()
-    {
-        Intent pindah_register = new Intent(MainActivity.this, Halaman_regis.class);
-        startActivity(pindah_register);
+
+
+//        register.setOnClickListener((new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                halamanRegister();
+//            }
+//        }));
+//
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Control khusus Admin login
+//                    if(nama.getText().toString().equals("Reza")) {
+//                        Intent admin = new Intent(MainActivity.this, Halaman_admin.class);
+//                        admin.putExtra("Nama", nama.getText().toString());
+//                        startActivity(admin);
+//                    }
+//                 else
+//                    {
+//                    halamanhome();
+//                }
+//            }
+//        });
+//    }
+//    public void halamanRegister()
+//    {
+//        Intent pindah_register = new Intent(MainActivity.this, Halaman_regis.class);
+//        startActivity(pindah_register);
+//    }
+//    public void halamanhome()
+//    {
+//        if(nama.length()==0 )
+//        {
+//            nama.requestFocus();
+//            nama.setError("Masukkan nama anda");
+//
+//
+//        }
+//        else if(pass_ku.length()==0)
+//        {
+//            pass_ku.requestFocus();
+//            pass_ku.setError("Masukkan Password anda");
+//        }
+//        else
+//        {
+//            Intent home = new Intent(MainActivity.this, Halaman_home.class);
+//            home.putExtra("Nama", nama.getText().toString());
+//            startActivity(home);
+//
+//
+//        }
+//
+//        }
     }
     public void halamanhome()
     {
-        if(nama.length()==0 )
-        {
-            nama.requestFocus();
-            nama.setError("Masukkan nama anda");
-
-
-        }
-        else if(pass_ku.length()==0)
-        {
-            pass_ku.requestFocus();
-            pass_ku.setError("Masukkan Password anda");
-        }
-        else
-        {
-            Intent home = new Intent(MainActivity.this, Halaman_home.class);
-            home.putExtra("Nama", nama.getText().toString());
-            startActivity(home);
-
-
-        }
-
-        }
+        Intent home = new Intent(MainActivity.this, Halaman_home.class);
+        startActivity(home);
+    }
 }
