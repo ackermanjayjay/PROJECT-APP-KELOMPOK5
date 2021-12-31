58e38a3ebbce00b1506bf54b0fa853111a8b0890
ackermanjayjay <reza23198@gmail.com>
2021-12-31 08:46:12 +0700

    supaya data harga unit barang terinput ke firebase

--- Coba_Firebase/app/src/main/java/com/example/coba_firebase/Pelanggan.java
+++ Coba_Firebase/app/src/main/java/com/example/coba_firebase/Pelanggan.java
@@ -1,16 +1,50 @@
 package com.example.coba_firebase;
 
-public class Pelanggan
+import android.content.Intent;
+import java.io.Serializable;
+
+public class Pelanggan implements Serializable
 {
     private String nama;
     private String alamat;
+    private String barang;
+    private String unit;
+    private String harga;
     private String key;
+
 
     public Pelanggan() {
 
 
 
     }
+    public String getBarang() {
+        return barang;
+    }
+
+    public void setBarang(String barang) {
+        this.barang = barang;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+
+
+
 
     public String getNama() {
         return nama;
@@ -38,11 +72,17 @@ public class Pelanggan
     @Override
     public String toString() {
         return " "+nama+"\n" +
-                " "+alamat +"\n";
+                " "+alamat +"\n"+
+                " "+harga+"\n"+
+                " "+barang+"\n"+
+                " "+unit;
     }
 
-    public Pelanggan(String nm, String almt){
+    public Pelanggan(String nm, String almt,String hr,String un,String br){
         nama = nm;
         alamat = almt;
+        harga=hr;
+        barang=br;
+        unit=un;
     }
 }
