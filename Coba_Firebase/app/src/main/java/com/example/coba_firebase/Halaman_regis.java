82d09e789425f76a579da76ec9889ba752bdda46
ackermanjayjay <reza23198@gmail.com>
2021-12-31 08:44:31 +0700

    memperbaharui supaya bisa list view barang

--- Coba_Firebase/app/src/main/java/com/example/coba_firebase/Halaman_regis.java
+++ Coba_Firebase/app/src/main/java/com/example/coba_firebase/Halaman_regis.java
@@ -9,13 +9,21 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Halaman_regis extends AppCompatActivity {
     private DatabaseReference database;
@@ -23,7 +31,16 @@ public class Halaman_regis extends AppCompatActivity {
     private Button btSubmit;
     private EditText etNama;
     private EditText etAlamat;
-//    private EditText etHarga;
+   private EditText etHarga;
+    private EditText etUnit;
+    private Spinner etBarang;
+    private TextView br;
+    private List show;
+
+    private String[] Barang = {"Telur","Minyak"};
+    String barangku;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +48,40 @@ public class Halaman_regis extends AppCompatActivity {
         setContentView(R.layout.halaman_regis);
         etNama = (EditText) findViewById(R.id.et_namabarang);
         etAlamat = (EditText) findViewById(R.id.et_alamat);
+        etHarga=(EditText)findViewById(R.id.et_harga);
+        etUnit=(EditText)findViewById(R.id.et_unit);
+        etBarang=(Spinner) findViewById(R.id.et_barang);
+        br=(TextView)findViewById(R.id.et_br);
         btSubmit = (Button) findViewById(R.id.bt_simpan);
 
         database = FirebaseDatabase.getInstance().getReference();
 //        getSupportActionBar().setTitle("Halaman Register");
+         ArrayAdapter adapter = new ArrayAdapter (this,
+                android.R.layout.simple_spinner_dropdown_item,Barang);
+         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            etBarang.setAdapter(adapter);
+
+        etBarang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView adapterView, View view, int i, long l) {
+           barangku=etBarang.getSelectedItem().toString();
+           br.setText(barangku);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView adapterView) {
+
+            }
+        });
+
 
         btSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!isEmpty(etNama.getText().toString()) && !isEmpty(etAlamat.getText().toString()))
-                    submitBarang(new Pelanggan(etNama.getText().toString(), etAlamat.getText().toString()));
+
+                    submitBarang(new Pelanggan(etNama.getText().toString(), etAlamat.getText().toString(),etHarga.getText().toString(),etUnit.getText().toString(),br.getText().toString()));
+
                 else
                     Snackbar.make(findViewById(R.id.bt_simpan), "Data barang tidak boleh kosong", Snackbar.LENGTH_LONG).show();
 
@@ -68,17 +109,19 @@ public class Halaman_regis extends AppCompatActivity {
             public void onSuccess(Void aVoid) {
                 etNama.setText("");
                 etAlamat.setText("");
-//                etHarga.setText("");
+                etHarga.setText("");
+                br.setText("");
+                etUnit.setText("");
                 Snackbar.make(findViewById(R.id.bt_simpan), "Data berhasil ditambahkan", Snackbar.LENGTH_LONG).show();
-                HalamanBeli();
+//                HalamanBeli();
             }
         });
     }
-    public void HalamanBeli()
-    {
-        Intent pindah = new Intent(Halaman_regis.this,Halaman_beli.class);
-        startActivity(pindah);
-    }
+//    public void HalamanBeli()
+//    {
+//        Intent pindah = new Intent(Halaman_regis.this,Data_pelanggan.class);
+//        startActivity(pindah);
+//    }
     public static Intent getActIntent(Activity activity) {
         // kode untuk pengambilan Intent
         return new Intent(activity, Halaman_regis.class);
